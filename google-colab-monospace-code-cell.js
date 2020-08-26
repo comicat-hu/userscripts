e6aedbdfe8f36efb2944f6fc88fe7d0bc1884e06
comicat-hu <senline39518@gmail.com>
2020-08-26 14:55:45 +0800

    Add google-colab-monospace-code-cell script

---
+++ google-colab-monospace-code-cell.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Google Colab Monospace Code Cell
+// @namespace    https://github.com/comicat-hu
+// @version      0.1
+// @description  set font to 'Consolas' in code cell
+// @author       comicat
+// @match        https://colab.research.google.com/drive/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/comicat-hu/userscripts/master/google-colab-monospace-code-cell.js
+// @downloadURL  https://raw.githubusercontent.com/comicat-hu/userscripts/master/google-colab-monospace-code-cell.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(function() {
+        let viewlines = document.querySelectorAll('div.view-lines');
+        if (viewlines) {
+            viewlines.forEach(function (el) {
+                el.style.fontFamily = 'Consolas';
+            });
+        }
+    }, 2000);
+})();
